c4ad0985fe5a12517fe1359ef103c958b6fc1738
yuri <ur24.ivanov@yandex.ru>
2023-12-16 21:17:46 +0300

    add new activity

--- app/src/main/java/com/eturn/EnterActivity.kt
+++ app/src/main/java/com/eturn/EnterActivity.kt
@@ -4,16 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.eturn.R
+import android.widget.EditText
 
-class StartActivity : AppCompatActivity() {
+class EnterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
-        val ToMainScreenBtn = findViewById<Button>(R.id.ToMainScreenBtn)
-        ToMainScreenBtn.setOnClickListener {
-            val intent = Intent(this, EnterActivity::class.java)
+        setContentView(R.layout.activity_enter)
+        val EnterButton : Button = findViewById(R.id.EnterBtn)
+
+        EnterButton.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
